aac6467c32db7afe7f3fbb43e2fb75089b63591a
JVsilveira <joaovitor.santossilveira@gmail.com>
2023-05-09 10:26:41 -0300

    ajustes

--- src/components/me/me.jsx
+++ src/components/me/me.jsx
@@ -13,17 +13,19 @@ export default () => (
     <div className="areaTexto">
       <div className="sobre">
         <p>
-          Nasci no dia 07/03/2003, na cidade de Guaíba-RS. Descobri a paixão
-          pela programação e desde então venho estudando com o objetivo de
-          desempenhar com sucesso nessa profissão. Tenho a ambição de aprender
-          cada vez mais, já tendo formação no curso de nível técnico em
-          informática da escola Dr. Solon Tavares e no terceiro semestre do
-          bacharelado de Ciência da Computação na Faculdade de Desenvolvimento
-          do Rio Grande do Sul (FADERGS). Atualmente, desenvolvo diversos
-          projetos em Javascript, CSS, HTML e JAVA para fins de aprendizado e
-          aprimoramento das técnicas. Estou à procura de uma primeira
-          oportunidade como desenvolvedor Front-End, apesar de também poder
-          atuar na área do Back-End.
+          Nascido em 07 de março de 2003, na cidade de Guaíba-RS, atuei como
+          Suporte TI nos Registros Públicos de Barra do Ribeiro por 2 anos e 2
+          meses, realizando manutenção de computadores, servidores e bancos de
+          dados. Desde então, tenho me dedicado ao estudo da programação com o
+          objetivo de desempenhar com sucesso nesta profissão. Possuo formação
+          técnica em informática pela escola Dr. Solon Tavares e atualmente
+          estou cursando o terceiro semestre do Bacharelado em Ciência da
+          Computação na Faculdade de Desenvolvimento do Rio Grande do Sul
+          (FADERGS). Desenvolvo diversos projetos em JavaScript, CSS, HTML e
+          Java utilizando como ferramentas o React, Angular e Vue, a fim de
+          aprimorar minhas habilidades e conhecimentos. Busco uma oportunidade
+          como desenvolvedor Front-End, embora também possa atuar na área de
+          Back-End.
         </p>
       </div>
     </div>
