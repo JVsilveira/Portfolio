4f58e91b8536f49f5e36117e37954520db9ba1db
JVsilveira <joaovitor.santossilveira@gmail.com>
2023-05-09 16:16:36 -0300

    ajuste

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,7 +3,6 @@ import Main from '../templates/Main'
 import './contact.css'
 import Insta from '../../assets/imgs/insta.png'
 import Linkedin from '../../assets/imgs/linkedin.png'
-import Email from '../../assets/imgs/email.png'
 import Github from '../../assets/imgs/github.png'
 
 export default () => (
@@ -18,9 +17,6 @@ export default () => (
       </a>
       <a href="http://www.linkedin.com/in/jo%C3%A3o-vitor-dos-santos-silveira/">
         <img src={Linkedin} className="linkedin" />
-      </a>
-      <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJvqsgThggTVRfgrVxVjPBjqckfXXfRzVFktRTmrsqpswwmMGLzhBNQlQjsPrSNJFSwpWbq">
-        <img src={Email} className="email" />
       </a>
       <a href="https://github.com/JVsilveira">
         <img src={Github} className="github" />
